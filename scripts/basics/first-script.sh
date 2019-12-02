b213a61178f62cabb4bf0d823c8d90283fcb3a99
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2019-12-02 09:06:40 -0300

    First simple scripts

---
+++ scripts/basics/first-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "First message"
+
+# First option to call other scripts
+. /home/gabriel.negreiros/projects/shell-script-studies/scripts/basics/include-first-script.sh
+# Second option
+source  /home/gabriel.negreiros/projects/shell-script-studies/scripts/basics/include-first-script.sh
+
